9523de3a56713d39f68f26fd67def3b622241c00
Ricardo Guerra <RikkuG491@gmail.com>
2023-10-12 15:26:25 -0400

    Added solution code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 // Your code here
+
+const restaurantMenuDivElement = document.getElementById('restaurant-menu')
+const burgerImageElement = document.getElementById('image')
+const burgerNameElement = document.getElementById('name')
+const burgerNumberInCartElement = document.getElementById('number-in-cart-count')
+
+fetch('http://localhost:3000/burgers')
+.then(response => response.json())
+.then(burgers => {
+
+    // displayBurgerDetails(burgers[0])
+
+    burgers.forEach(burger => {
+        addBurgerNameToRestaurantMenu(burger)
+    })
+})
+
+// Deliverable # 1
+function addBurgerNameToRestaurantMenu(burger){
+    const spanElement = document.createElement('span')
+    spanElement.textContent = burger.name
+    restaurantMenuDivElement.appendChild(spanElement)
+
+    // Deliverable # 2
+    spanElement.addEventListener('click', () => {
+        displayBurgerDetails(burger)
+    })
+}
+
+function displayBurgerDetails(burger){
+    burgerImageElement.src = burger.image
+    burgerNameElement.textContent = burger.name
+    burgerNumberInCartElement.textContent = burger.number_in_cart
+}
+
+// Deliverable # 3
+const addToCartForm = document.getElementById('add-to-cart-form')
+addToCartForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const numberToAddInputElement = document.getElementById('number-to-add')
+    const sum = Number(burgerNumberInCartElement.textContent) + Number(numberToAddInputElement.value)
+    burgerNumberInCartElement.textContent = sum
+})
